09ca00f37d53b9d0e83f500c002e33c4db4d62f6
Dowland Aiello <dowlandaiello@gmail.com>
2021-04-12 16:15:18 -0400

    auth: Finish RegisterAlias.
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- src/hub/auth.rs
+++ src/hub/auth.rs
@@ -24,6 +24,7 @@ impl fmt::Display for AuthError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::AliasTaken => write!(f, "username taken"),
+            Self::SessionNonexistent => write!(f, "session does not exist"),
         }
     }
 }
@@ -33,7 +34,7 @@ impl Error for AuthError {}
 #[derive(Default)]
 pub struct Authenticator {
     // The usernames claimed by an individual with a particular email
-    user_aliases: HashMap<String, HashSet<Rc<String>>>,
+    user_aliases: HashMap<Rc<String>, HashSet<Rc<String>>>,
 
     // All of the usernames claimed by users
     claimed_usernames: HashSet<Rc<String>>,
@@ -52,15 +53,29 @@ impl Actor for Authenticator {
 impl Handler<RegisterAlias> for Authenticator {
     type Result = Result<(), AuthError>;
 
-    fn handle(&mut self, msg: RegisterAlias, ctx: &mut Self::Context) -> Result<(), AuthError> {
+    fn handle(&mut self, msg: RegisterAlias, _ctx: &mut Self::Context) -> Result<(), AuthError> {
         if self.claimed_usernames.contains(&msg.0) {
             return Err(AuthError::AliasTaken);
         }
 
-        let email = self.sessions.get(&msg.1).ok_or(AuthError::SessionNonexistent)?;
-        let username = Rc::new(msg.0);
+        match msg {
+            RegisterAlias(username, session) => {
+                // Don't clone the username around, share a copy between reprs
+                let shared_username: Rc<String> = Rc::new(username);
+                let email = self.sessions.get(&session).ok_or(AuthError::SessionNonexistent)?;
 
-        self.claimed_usernames.insert(username.clone());
-        self.user_aliases.entry(email).or_insert([]);
+                self.claimed_usernames.insert(shared_username.clone());
+
+                // If the user already has some usernames registered, simply add the username to
+                // those. Otherwise, create the new record with just the one username.
+                if let Some(existing_aliases) = self.user_aliases.get_mut(email) {
+                    existing_aliases.insert(shared_username);
+                } else {
+                    self.user_aliases.insert(email.clone(), vec![shared_username].into_iter().collect());
+                }
+            }
+        }
+
+        Ok(())
     }
 }
