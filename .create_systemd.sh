ace3ec722448a66482bf44586b298e32f44da410
Dowland Aiello <dowlandaiello@gmail.com>
2021-04-20 18:15:45 -0400

    update .create_systemd.sh
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- .create_systemd.sh
+++ .create_systemd.sh
@@ -7,6 +7,10 @@ After=network-online.target
 WantedBy=multi-user.target
 
 [Service]
+Environment=TZ_CLIENT_ID=$TZ_CLIENT_ID
+Environment=TZ_SECRET=$TZ_SECRET
+Environment=SSL_CERT_PATH=$SSL_CERT_PATH
+Environment=SSL_KEY_PATH=$SSL_KEY_PATH
 Type=simple
 WorkingDirectory=$(pwd)
 ExecStart=$HOME/.cargo/bin/cargo run" > ~/.local/share/systemd/user/tzchat.service
